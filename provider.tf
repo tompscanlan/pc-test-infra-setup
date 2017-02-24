9d59c215073672fb625fa4cab51016a1d8ba3ee7
Arslan Abbasi <abbasia@vmware.com>
2017-02-24 19:16:04 +0000

    removing old files

--- provider.tf
+++
@@ -1,9 +0,0 @@
-# Configure the OpenStack Provider
-provider "openstack" {
-    domain_id = "${var.openstack_domain_id}"
-    user_name  = "${var.openstack_user_name}"
-    tenant_name = "${var.openstack_tenant_name}"
-    password  = "${var.openstack_password}"
-    auth_url  = "${var.openstack_auth_url}"
-    insecure = "${var.openstack_insecure}"
-}
