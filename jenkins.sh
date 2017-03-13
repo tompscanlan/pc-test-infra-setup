9364fbc5bd31319f7d721f17aab596b1bc62ea5b
Tom Scanlan <tscanlan@vmware.com>
2017-03-13 15:26:37 -0400

    add jenkins script

---
+++ jenkins.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+pwd
+
+echo "Setup terraform"
+wget -N -nv https://releases.hashicorp.com/terraform/0.8.6/terraform_0.8.6_linux_amd64.zip
+unzip -o terraform_0.8.6_linux_amd64.zip
+
+tf=`pwd`/terraform
+chmod +x $tf
+
+echo "run terraform"
+cd build_pipeline/infra
+$tf get
+$tf plan
+git status
